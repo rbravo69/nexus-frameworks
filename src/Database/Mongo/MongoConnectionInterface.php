d75f8392e97e6424f11ee372a5d57ba7baecf479
Rafael Antonio Bravo Sosa <rbravo69@gmail.com>
2026-09-02 16:07:04 -0600

    feat(mongodb): add document store contract

---
+++ src/Database/Mongo/MongoConnectionInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nexus\Database\Mongo;
+
+interface MongoConnectionInterface
+{
+    /** @param array<string, mixed> $document */
+    public function insert(string $collection, array $document): string;
+
+    /**
+     * @param array<string, mixed> $filter
+     * @return list<array<string, mixed>>
+     */
+    public function find(string $collection, array $filter = []): array;
+
+    /**
+     * @param array<string, mixed> $filter
+     * @param array<string, mixed> $update
+     */
+    public function update(string $collection, array $filter, array $update): int;
+
+    /** @param array<string, mixed> $filter */
+    public function delete(string $collection, array $filter): int;
+
+    /** @param array<string, mixed> $keys */
+    public function createIndex(string $collection, array $keys, bool $unique = false): string;
+}
